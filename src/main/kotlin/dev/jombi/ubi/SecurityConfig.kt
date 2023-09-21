0e04d00298cc8f6841f352c921d22f091d3e53af
jombi <jombi@duck.com>
2023-09-21 16:16:22 +0900

    cleared import

--- src/main/kotlin/dev/jombi/ubi/SecurityConfig.kt
+++ src/main/kotlin/dev/jombi/ubi/SecurityConfig.kt
@@ -1,15 +1,12 @@
 package dev.jombi.ubi
 
 import dev.jombi.ubi.filter.TokenFilter
-import dev.jombi.ubi.service.UserDetailsServiceImpl
 import dev.jombi.ubi.util.jwt.TokenFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
