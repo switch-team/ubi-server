4157c691bbc38f2960ebfdb94501af97c85bcba8
jombi <jombi@duck.com>
2023-10-07 10:58:12 +0900

    More enhancements for friend - compressed SEND AND RECEIVE to PENDING - removed reject friend request. (use delete to reject)

--- src/main/kotlin/dev/jombi/ubi/controller/FriendController.kt
+++ src/main/kotlin/dev/jombi/ubi/controller/FriendController.kt
@@ -1,7 +1,7 @@
 package dev.jombi.ubi.controller
 
 import dev.jombi.ubi.dto.request.UserIdRequest
-import dev.jombi.ubi.dto.response.UserIdAndNameResponse
+import dev.jombi.ubi.dto.response.PendingResponse
 import dev.jombi.ubi.dto.response.UserListResponse
 import dev.jombi.ubi.service.FriendService
 import dev.jombi.ubi.service.UserService
@@ -14,38 +14,28 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
 
 @RestController
 @RequestMapping("/friend")
 class FriendController(val service: FriendService, val userService: UserService) {
-    @GetMapping("/list")
+    @GetMapping("/")
     fun list(auth: Authentication): ResponseEntity<GuidedResponse<UserListResponse>> {
         val user = userService.getUserById(UUID.fromString(auth.name))
-//            ?: throw CustomError(ErrorDetail.USER_NOT_FOUND) // 회원 탈퇴 후에도 JWT 토큰이 남아있을 가능성 존재
         val result = service.getFriendList(user)
         return ResponseEntity.ok(GuidedResponseBuilder { message = "found" }.build(result))
     }
 
-
-    //초대 건거
-    @GetMapping("/invited-list")
-    fun invitedList(auth: Authentication): ResponseEntity<GuidedResponse<UserListResponse>> {
+    @GetMapping("/pending")
+    fun invitedList(auth: Authentication): ResponseEntity<GuidedResponse<List<PendingResponse>>> {
         val user = userService.getUserById(UUID.fromString(auth.name))
-        val result = service.getInvitedList(user)
-        return ResponseEntity.ok(GuidedResponseBuilder { message = "found" }.build(result))
-    }
-    @GetMapping("/received-list")
-    fun receivedList(auth: Authentication): ResponseEntity<GuidedResponse<UserListResponse>> {
-        val user = userService.getUserById(UUID.fromString(auth.name))
-        val result = service.getReceivedList(user)
+        val result = service.getPending(user)
         return ResponseEntity.ok(GuidedResponseBuilder { message = "found" }.build(result))
     }
 
-    @PostMapping("/invite")
-    fun invite(@RequestBody request: UserIdRequest, auth: Authentication): ResponseEntity<GuidedResponse<Any>> {
+    @PostMapping("/request")
+    fun request(@RequestBody request: UserIdRequest, auth: Authentication): ResponseEntity<GuidedResponse<Any>> {
         val invitedUser = userService.getUserByPhoneOrEmail(request.id)
         val user = userService.getUserById(UUID.fromString(auth.name))
         service.inviteFriend(invitedUser, user)
@@ -60,14 +50,6 @@ class FriendController(val service: FriendService, val userService: UserService)
         return ResponseEntity.ok(GuidedResponseBuilder { message = "succes" }.noData())
     }
 
-    @PostMapping("/reject")
-    fun reject(@RequestBody request: UserIdRequest, auth: Authentication): ResponseEntity<GuidedResponse<Any>> {
-        val invitedUser = userService.getUserByPhoneOrEmail(request.id)
-        val user = userService.getUserById(UUID.fromString(auth.name))
-        service.deleteFriend(invitedUser, user)
-        return ResponseEntity.ok(GuidedResponseBuilder { message = "succes" }.noData())
-    }
-
     @DeleteMapping("/delete")
     fun delete(@RequestBody request: UserIdRequest, auth: Authentication): ResponseEntity<GuidedResponse<Any>> {
         val deleteFriend = userService.getUserByPhoneOrEmail(request.id)
@@ -75,19 +57,4 @@ class FriendController(val service: FriendService, val userService: UserService)
         service.deleteFriend(deleteFriend, user)
         return ResponseEntity.ok(GuidedResponseBuilder { message = "succes" }.noData())
     }
-//    @PostMapping("/invite")
-//    fun invite(@RequestBody request: FriendInviteRequest, auth: Authentication): ResponseEntity<GuidedResponse<Any>> {
-//        val invitedUser = userService.getUserByPhoneOrEmail(request.id)
-//        val user = userService.getUserById(UUID.fromString(auth.name))
-//        service.inviteFriend(invitedUser, user)
-//        return ResponseEntity.ok(GuidedResponseBuilder { message = "secces" }.noData())
-//    }
-//
-//    @PostMapping("/recive")
-//    fun recive(@RequestBody request: FriendReciveRequest, auth: Authentication): ResponseEntity<GuidedResponse<Any>> {
-//        val recivedUser = userService.getUserByPhoneOrEmail(request.id)
-//        val user = userService.getUserById(UUID.fromString(auth.name))
-//        service.reciveFriend(recivedUser, user)
-//        return ResponseEntity.ok(GuidedResponseBuilder { message = "recived" }.noData())
-//    }
 }
